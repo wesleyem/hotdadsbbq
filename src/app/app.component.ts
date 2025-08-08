d53163a959f0520cdbb989af622a2613465311b0
wesleyem <wesleyem@pm.me>
2025-08-08 13:50:01 -0700

    feat: add toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, MatToolbarModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
